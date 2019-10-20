6168847206b7879e0ef7271550b7153a52023d4d
thesinsay9 <nick.guschin1998@yandex.ru>
2019-10-20 23:50:59 +0300

    solution_after_contest_pass

---
+++ task_1_1/cmake-build-debug/CMakeFiles/untitled59.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled59.dir/main.cpp.o"
+  "untitled59.pdb"
+  "untitled59"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled59.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
